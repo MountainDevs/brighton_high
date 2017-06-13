3bb99ca60caf95a94ec602c4e61fa162df5353f0
Jon Myrick <jonmyrick7401@gmail.com>
2017-06-13 17:32:30 -0600

    added server

---
+++ server.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+app.use(express.static('./build'))
+app.use(bodyParser.json());
+app.listen(3000, function () {
+  console.log(`listening on port ${this.address().port}`);
+})
