1a9618327f2e12d680656274aa9c9e7acad71f12
Sterling Chin <sterling.chin@gmail.com>
2017-06-16 23:01:54 -0600

    updating master

--- src/App.js
+++ src/App.js
@@ -1,41 +1,60 @@
 import React, { Component } from 'react';
+import { Tabs } from 'bitts'
 import logo from './logo.svg';
-import { Styles, Buttons } from 'bitts';
+import './App.css';
 
-const Landing = React.createClass({
+class App extends Component {
   render() {
+    const styles = this.styles()
     return (
       <div style={styles.app}>
+        <Tabs
+          tabs={[
+            {
+              value: 'tab1',
+              label: 'Tab 1'
+            },
+            {
+              value: 'tab2',
+              label: 'Tab 2'
+            },
+            {
+              value: 'tab3',
+              label: 'Tab 3'
+            }
+          ]}
+        />
         <div style={styles.app_header}>
           <img src={logo} style={styles.app_logo} alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p style={styles.app_intro}>
-          To get started, edit <code>src/app.js</code> and save to reload.
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
-    );
-  },
+    )
+  }
 
-  styles () {
+  styles() {
     return {
       app: {
         textAlign: 'center'
       },
       app_logo: {
-        animation: 'app-logo-spin infinite 20s linear',
+        animation: 'App-logo-spin infinite 20s linear',
         height: 80
       },
       app_header: {
-        backgroundColor: Styles.Colors.BLACK,
+        backgroundColor: '#222',
         height: 150,
         padding: 20,
-        color: Styles.Colors.WHITE
+        color: 'white'
       },
       app_intro: {
-      fontSize: Styles.FontSizes.XLARGE,
+        fontSize: 'large'
       }
     }
   }
-})
-export default Landing;
+}
+
+export default App;
