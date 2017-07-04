609f0e3f0f5fc7454007c69022a3dafa4cc5f38f
Jon Myrick <jonmyrick7401@gmail.com>
2017-07-04 14:53:29 -0600

    database is configured

--- server.js
+++ server.js
@@ -4,9 +4,20 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const massive = require('massive');
+const config = require('./config');
 
 app.use(express.static('./build'))
 app.use(bodyParser.json());
+
+let db = massive.connectSync({
+    connectionString: config.databaseString
+  })
+app.set('db', db);
+db.set_user((err, data) => {
+  if (err) console.log(err);
+  else console.log('All tables successfully reset');
+});
 
 var port = process.env.PORT || 8080;
 app.listen(port, function () {
