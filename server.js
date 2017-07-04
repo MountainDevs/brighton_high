03caef0a21316e8e68ae15f5632747768e280fb1
Jon Myrick <jonmyrick7401@gmail.com>
2017-07-04 15:04:28 -0600

    added controller

--- server.js
+++ server.js
@@ -1,23 +1,20 @@
 "use strict";
 
 const express = require('express');
-const app = express();
+const app = module.exports = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const massive = require('massive');
 const config = require('./config');
+const db = massive.connectSync({
+    connectionString: config.databaseString
+  })
 
 app.use(express.static('./build'))
 app.use(bodyParser.json());
-
-let db = massive.connectSync({
-    connectionString: config.databaseString
-  })
 app.set('db', db);
-db.set_user((err, data) => {
-  if (err) console.log(err);
-  else console.log('All tables successfully reset');
-});
+
+const controller = require('./controller')
 
 var port = process.env.PORT || 8080;
 app.listen(port, function () {
