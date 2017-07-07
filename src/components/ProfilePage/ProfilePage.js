6dc51079e65c0a4b548c7c4eb5b10b27cf5cb09a
Steven Nagie <sanagie@yahoo.com>
2017-07-06 18:32:18 -0600

    There might be a god
    Super easy to get react component to do upload stuff.

--- src/components/ProfilePage/ProfilePage.js
+++ src/components/ProfilePage/ProfilePage.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import FileUpload from './FileUpload';
 
 class ProfilePage extends Component {
     render() {
         return (
             <div style={{'color': 'white', 'margin': '20px', 'font-size': '20px'}}>
                 Classmate Profiles will be Available July 15, 2017
+            <FileUpload></FileUpload>
             </div>
         );
     }
