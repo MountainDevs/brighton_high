3afa65cc0021dae7612ba98a3aa786c46e5b9598
Gustav Nordstrom <gnordstrom@users.noreply.github.com>
2017-07-15 13:07:23 -0600

    Messing with header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,9 +5,10 @@ import './Header.css';
 class Header extends Component {
     render() {
         return (
-            <div>
-                Header Component
-            </div>
+            <header className="header-wrapper">
+                <div>Brighton High Class of 1987</div>
+                <div>Login Button</div>
+            </header>
         );
     }
 }
