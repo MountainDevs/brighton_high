1654af5a49f8b65242b7e72b9ae56c0199fb64bb
Gustav Nordstrom <gnordstrom@users.noreply.github.com>
2017-07-16 16:00:42 -0600

    cleaning

--- src/components/pages/Profiles.js
+++ src/components/pages/Profiles.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
-
 // Components
 import Card from './Shared/Card';
 import Title from './Shared/Title';
-
 // Images
 import prof_pic from '../../assets/profile_pic.png'
 // CSS
@@ -13,12 +11,6 @@ class Profiles extends Component {
     render() {
         return (
             <div className="profiles-wrapper">
-                {/* <div className="profiles-top">
-                     <div className="details-top-title">
-                        Classmate Profiles
-                     </div>
-                </div>  */}
-
                 <Title title="Classmate Profiles"/>
 
                 <div className="profiles-table">
@@ -26,7 +18,7 @@ class Profiles extends Component {
                         <div>Name</div>
                         <div>Attending?</div>
                     </div>
-
+                    {/* Placeholders until mapping over data and outputting Cards  */}
                     <Card name="Gustav Nordstrom" imageUrl={prof_pic} attending="Yes"/>
                     <Card name="Jon Myrick" imageUrl={prof_pic} attending="Yes"/>
                     <Card name="Steven Nagie" imageUrl={prof_pic} attending="Yes"/>
