a20aa57a5e47cfea1f10f214fbc91af1db77b823
Jon Myrick <jonmyrick7401@gmail.com>
2017-07-16 13:25:48 -0600

    finished state managment for register flow

---
+++ src/dataService.js
@@ -0,0 +1,20 @@
+
+let userData = {
+  email: '',
+  password: '',
+  firstName: '',
+  lastName: '',
+  middleName: '',
+  email: '',
+  phone: '',
+  phone: '',
+  address: '',
+  city: '',
+  state: '',
+  zipcode: '',
+  bio: ''
+}
+
+module.exports = {
+  userData
+}
