7bf910b4cfce7688459803e8eabf5904793cec34
Jon Myrick <jonmyrick7401@gmail.com>
2017-07-16 14:07:35 -0600

    started service layer

--- src/dataService.js
+++ src/dataService.js
@@ -1,3 +1,6 @@
+import axios from 'axios';
+
+const BASEURL = '';
 
 let userData = {
   email: '',
@@ -15,6 +18,45 @@ let userData = {
   bio: ''
 }
 
+function login(email, password) {
+  axios.post(`${BASEURL}`)
+}
+
+function logout() {
+  // to be determined
+}
+
+function postUser(user) {
+  axios.post(`${BASEURL}/post_user`)
+}
+
+function updateUser(user) {
+  axios.post(`${BASEURL}/update_user`)
+}
+
+function postStripeRecord() {
+  axios.post(`${BASEURL}/post_stripe_record`)
+}
+
+function getUser(email) {
+  axios.get(`${BASEURL}/get_user`)
+}
+
+function getRegisterdUsers() {
+  axios.get(`${BASEURL}/get_registered_users`)
+}
+
+function getClassmates() {
+  axios.get(`${BASEURL}/get_classmates`)
+} 
+
 module.exports = {
-  userData
+  userData,
+  login,
+  logout,
+  postUser,
+  updateUser,
+  postStripeRecord,
+  getUser,
+  getClassmates
 }
