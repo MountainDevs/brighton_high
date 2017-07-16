52a13dee85004706ca69695f7bc0919a97ddc7df
Sterling Chin <sterling.chin@gmail.com>
2017-07-16 15:35:23 -0600

    adding tables

--- src/components/register/Pay.js
+++ src/components/register/Pay.js
@@ -11,7 +11,53 @@ class Pay extends Component {
                     <div className='pay-body'>
                         <p>Here we will explain a little about the fees and what they are paying for.</p>
                         <section className='stripe-code'>
-                            
+                            <h3>
+                                Early Registration
+                            </h3>
+                            <table style={styles.table}>
+                                <tr>
+                                    <td style={styles.table_description}>Alumni Only </td>
+                                    <td style={styles.table_cost}>$94</td>
+                                    <td style={styles.table_button}><StripeAlumniPre /></td>
+                                </tr>
+                                <tr>
+                                    <td style={styles.table_description}>Alumni + Spouse </td>
+                                    <td style={styles.table_cost}>$157</td>
+                                    <td style={styles.table_button}><StripeAlumniSpousePre /></td>
+                                </tr>
+                            </table>
+                            <br />
+                            <h3>
+                                Registration
+                            </h3>
+                            <table style={styles.table}>
+                                <tr>
+                                    <td style={styles.table_description}>Alumni Only </td>
+                                    <td style={styles.table_cost}>$104</td>
+                                    <td style={styles.table_button}><StripeAlumni /></td>
+                                </tr>
+                                <tr>
+                                    <td style={styles.table_description}>Alumni + Spouse </td>
+                                    <td style={styles.table_cost}>$167</td>
+                                    <td style={styles.table_button}><StripeAlumniSpouse /></td>
+                                </tr>
+                            </table>
+                            <br />
+                            <h3>
+                                Day Of Registration
+                            </h3>
+                            <table style={styles.table}>
+                                <tr>
+                                    <td style={styles.table_description}>Day Of - Alumni Only </td>
+                                    <td style={styles.table_cost}>$120</td>
+                                    <td style={styles.table_button}><StripeAlumniDayOf /></td>
+                                </tr>
+                                <tr>
+                                    <td style={styles.table_description}>Day Of - Alumni + Spouse </td>
+                                    <td style={styles.table_cost}>$183</td>
+                                    <td style={styles.table_button}><StripeAlumniSpouseDayOf /></td>
+                                </tr>
+                            </table>
                         </section>
                         <div className='pay-buttons'>
                             <Link to='/register/additional'>Back</Link>
