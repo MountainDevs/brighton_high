4f33f614319e805b640b7b973c404048947f7777
Gustav Nordstrom <gnordstrom@users.noreply.github.com>
2017-07-16 20:54:19 -0600

    changes

--- src/components/pages/Classmates.js
+++ src/components/pages/Classmates.js
@@ -2,10 +2,24 @@ import React, { Component } from 'react';
 // Components
 import Title from './Shared/Title';
 import CardClassmates from './Shared/CardClassmates';
+// Data
+import { getAllUsers } from '../../dataService.js';
 // CSS
 import './Classmates.css';
 
 class ClassMates extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+
+        }
+    }
+
+    ComponentDidMount() {
+        // getAllUsers();
+    }
+
     render() {
         return (
             <div className="component-wrapper">
