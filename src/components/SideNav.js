249c998a5d0a35ec68f1e772da303fad84e90bab
Steven Nagie <sanagie@yahoo.com>
2017-07-17 22:30:02 -0600

    Side nav photo and name will reflect whatever is in the dataService

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 // Components
 import NavHeader from './side_nav/NavHeader';
 import NavItems from './side_nav/NavItems';
+import { userData } from '../dataService.js';
 
 // Images
 import profile_pic from '../assets/profile_pic.png';
@@ -11,12 +12,20 @@ import profile_pic from '../assets/profile_pic.png';
 import './SideNav.css';
 
 class SideNav extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      name: userData.firstName,
+      photoSrc: userData.photo
+    }
+  }
     render() {
         return (
             <div className="sidenav-wrapper">
                 <NavHeader 
-                    name="Gustav"
-                    imageUrl={profile_pic}
+                    name={this.state.name || "Welcome!"}
+                    imageUrl={this.state.photoSrc ? "https://s3-us-west-2.amazonaws.com/brighton-high-1987/" + this.state.photoSrc : profile_pic}
                 />
                 
                 <NavItems />
