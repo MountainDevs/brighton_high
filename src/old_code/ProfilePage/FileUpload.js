d097dc900c4124f51ec8de62558b20b2c430241f
Steven Nagie <sanagie@yahoo.com>
2017-07-17 20:40:07 -0600

    Added messages for user in photo upload components

--- src/old_code/ProfilePage/FileUpload.js
+++ src/old_code/ProfilePage/FileUpload.js
@@ -10,26 +10,47 @@ const dataService = require('../../dataService');
    constructor(props) {
      super(props)
 
-     
+     this.state = {
+       step: 1
+     }
+    this.onDrop = this.onDrop.bind(this);
    }
 
   onDrop (files) {
+    this.setState({
+      step: 2
+    });
     superagent.post('http://localhost:5000/api/s3/upload')
     .attach('file', files[0], `${dataService.userData.id}_${dataService.userData.lastName}_${new Date()}`)
     .end((err, res) => {
       if (err) {
         console.log(err);
+        this.setState({step:4});
+        setTimeout(() => {this.setState({step:1})}, 3000);
       } else {
         dataService.changePhoto(JSON.parse(res.text).fileName);
+        this.setState({step:3});
+        setTimeout(() => {this.setState({step:1})}, 3000);
       }
     })
   }
 
   render(){
     return (
-        <div className={ 'margin-top-20 pointer ' +  (this.props.hideUpload ? 'hidden' : '') }>
+        <div className={ 'margin-top-20 pointer ' +  (this.props.hideUpload ? 'hidden' : 'flex-center') }>
           <Dropzone onDrop={this.onDrop} multiple={false}>
-            <div>Try dropping a file here, or click to select a file to upload.</div>
+            <div className={"full-height-width " + (this.state.step === 1 ? 'flex-center' : 'hidden')}>
+              Try dropping a file here, or click to select a file to upload.
+            </div>
+            <div className={"full-height-width " + (this.state.step === 2 ? 'flex-center' : 'hidden')}>
+              Uploading, just a moment...
+            </div>
+            <div className={"full-height-width " + (this.state.step === 3 ? 'flex-center' : 'hidden')}>
+              All done!
+            </div>
+            <div className={"full-height-width " + (this.state.step === 4 ? 'flex-center' : 'hidden')}>
+              Oh no, there's been an error! Please try again.
+            </div>
           </Dropzone>
         </div>
     );
