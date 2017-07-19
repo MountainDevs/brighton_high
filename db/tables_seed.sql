dfe936c11b2f1ac930468acbb10e67f4cc26742a
Steven Nagie <sanagie@yahoo.com>
2017-07-19 12:32:02 -0600

    User profile dummy text

--- db/tables_seed.sql
+++ db/tables_seed.sql
@@ -28,7 +28,10 @@ CREATE TABLE stripe_records (
 
 INSERT INTO users (first_name, last_name, email, password, permissions) VALUES
 ('Jon', 'Myrick', 'jon@gmail.com', '123', 'member'),
-('Sara', 'Johnson', 'sara@gmail.com', '123');
+('Sara', 'Johnson', 'sara@gmail.com', '123', null);
+
+INSERT INTO users (first_name, last_name, email, password, phone, address, city, state, zipcode, bio, attending, photo, permissions) VALUES
+('Gustav', 'Nordstrom', 'gus@gmail.com', '123', '2345678876', '123 Fake Street, apartment 27', 'Provo', 'Utah', '98765', 'This is the bio of a great man', true, '1_Myrick_Mon+Jul+17+2017+22%3A29%3A07+GMT-0600+(Mountain+Daylight+Time).jpeg', 'member');
 
 INSERT INTO stripe_records (record) VALUES
 ('this is a stripe record');
