0a512da4f4634f7c96271b4c44feb56accdb797d
Steven Nagie <sanagie@yahoo.com>
2017-07-19 22:13:34 -0600

    Commented out 'More Stuff' container in UserProfile.js

--- src/components/pages/UserProfile.js
+++ src/components/pages/UserProfile.js
@@ -59,14 +59,14 @@ class UserProfile extends Component {
                         </div>
                     </div>
 
-                    <div className="pcard">
+                    {/* <div className="pcard">
                         <div className="pcard-about">
                             <div className="pcard-about-title">More Stuff</div>
                             <div className="pcard-about-content">
                                 "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
                             </div>
                         </div>
-                    </div>
+                    </div> */}
 
                 </div>
             </div>
