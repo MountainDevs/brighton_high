275debad8dcee9effd0a59baadf58bd26c7bdc49
Steven Nagie <sanagie@yahoo.com>
2017-07-21 00:46:58 -0600

    Some styles for button

--- src/components/pages/MyProfile.js
+++ src/components/pages/MyProfile.js
@@ -9,6 +9,7 @@ import AboutForm from './MyProfile/AboutForm';
 import LoginRequest from '../login/LoginRequest';
 // CSS
 import './MyProfile.css';
+import '../../styles/common.css';
 
 class MyProfile extends Component {
   constructor(props) {
@@ -33,19 +34,17 @@ class MyProfile extends Component {
         return !this.props.loggedIn ? <LoginRequest></LoginRequest> :
         (
             <div className="myProfile-wrapper">
-              <div className="showProfileSelector">
-                {this.state.showProfile ? (
-                  <div>
-                    <p>You're currently displaying your profile information to your fellow alumni. If you'd like to hide, click here:</p>
-                    <button onClick={this.showProfileFalse}>Hide profile</button>
-                  </div>
-                ) : (
-                  <div>
-                    <p>You're currently hiding your profile information from your fellow alumni. Click below if you'd like to show them your information</p>
-                    <button onClick={this.showProfileTrue}>Show Profile</button>
-                  </div>
-                )}
-              </div>
+              {this.state.showProfile ? (
+                <div className="showProfile-wrapper">
+                  <p>You are currently displaying your profile information to your fellow alumni. If you would like to hide, click here:</p>
+                  <button className="blue-button" onClick={this.showProfileFalse}>Hide profile</button>
+                </div>
+              ) : (
+                <div className="showProfile-wrapper">
+                  <p>You are currently hiding your profile information from your fellow alumni. Click below if you would like to show them your information:</p>
+                  <button className="blue-button" onClick={this.showProfileTrue}>Show Profile</button>
+                </div>
+              )}
                 <TopInfo />
                 <ContactForm />
                 <AboutForm />
