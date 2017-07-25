5619f49da99990787708b12d201361c8e5fa3cf1
Sterling Chin <sterling.chin@gmail.com>
2017-07-24 21:30:19 -0600

    update header buttons

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,30 +5,47 @@ import './Header.css';
 
 class Header extends Component {
     render() {
+        const styles = this.styles()
         return (
             <header className="header-wrapper">
                 <div className="header-title">Brighton High Class of 1987</div>
                 <div className="header-login">
 
                     {!this.props.loggedIn ? (
-                        <Link to="/login" className="login-button">
-                            Login/Register
+                        <Link 
+                            to="/login" 
+                            style={styles.button}
+                            >
+                            Login / Register
                         </Link>
                     ) : null}
 
                     {this.props.loggedIn && !userData.stripe_token ? (
                         <div>
-                            <Link to="/register/pay" className="login-button" onClick={this.props.switchLogin.bind(null, false)}>
+                            <Link 
+                                to="/register/pay"
+                                onClick={this.props.switchLogin.bind(null, false)}
+                                style={styles.button}
+                            >
                                 Pay
                             </Link>
-                            <Link to="/" className="login-button" onClick={this.props.switchLogin.bind(null, false)}>
+                            <Link 
+                                to="/"
+                                onClick={this.props.switchLogin.bind(null, false)}
+                                style={styles.button}
+                            >
                                 Logout
                             </Link>
                         </div>
                     ): null }
 
                     {this.props.loggedIn && userData.stripe_token ? (
-                        <Link to="/" className="login-button" onClick={this.props.switchLogin.bind(null, false)}>
+                        <Link
+                            to="/" 
+                            className="login-button" 
+                            onClick={this.props.switchLogin.bind(null, false)}
+                            style={styles.button}
+                        >
                             Logout
                         </Link>
                     ) : null}
@@ -36,6 +53,23 @@ class Header extends Component {
                 </div>
             </header>
         );
+    }
+    styles () {
+        return {
+            button: {
+                border: 'none',
+                borderRadius: 3,
+                color: '#ffffff',
+                textAlign: 'center',
+                width: 75,
+                padding: 15,
+                background: '#537187',
+                cursor: 'pointer',
+                fontSize: 12,
+                marginLeft: 20,
+                padding: 13,
+            }
+        }
     }
 }
 
