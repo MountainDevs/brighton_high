215b4850fd72ffd61fde0d1cd30fee31951ecc07
Steven Nagie <sanagie@yahoo.com>
2017-07-24 18:43:39 -0600

    Erased comments from StripeComponent and got rid of amount in State because it isn't necessary

--- src/components/Stripe/StripeComponent.js
+++ src/components/Stripe/StripeComponent.js
@@ -7,7 +7,6 @@ class StripeComponent extends Component {
     super(props);
 
     this.state = {
-      amount: 9400,
       id: userData.id,
       email: userData.email,
       password: userData.password,
@@ -15,12 +14,9 @@ class StripeComponent extends Component {
       lastName: userData.lastName,
       middleName: userData.middleName
     }
-    this.userPaid = this.userPaid.bind(this);
   }
 
   componentWillMount() {
-    // login(this.state.email, this.state.password).then(data => {
-    // });
       this.setState({
         id: userData.id,
         email: userData.email,
@@ -29,52 +25,17 @@ class StripeComponent extends Component {
         lastName: userData.lastName,
         middleName: userData.middleName
       });
-      console.log("component will mount", this.state);
-    // checkUser().then(data => {
-    //   this.setState({
-    //     id: userData.id,
-    //     email: userData.email,
-    //     password: userData.password,
-    //     firstName: userData.firstName,
-    //     lastName: userData.lastName,
-    //     middleName: userData.middleName
-    //   });
-    // })
-    // .catch(err => {
-    //   console.log("Error checking data");
-    // });
   }
 
   onToken = (token) => {
     token.amount = this.props.amount;
-    console.log("On token state: ", this.state);
-    console.log("On token data: ", userData);
     token.chargeDescription = `${this.state.email}`;
     sendToStripe(token)
       .then(response => {
         alert("Payment successful");
-        console.log(response.data);
-        this.userPaid(response.data.stripeData.id);
-            //Catch throwing even when successful
-            // .catch(alert("Payment was successful, but there was a problem registering your account. Please contact Jessica@brightonhigh1987.com"));
+        userData.stripe_token = response.data.stripeData.id;
       })
       .catch(err => {console.log(err)});
-      //Somehow both the then and catch are being thrown. Don't know what to do about that. 
-      // .catch(alert("There was a problem processing your payment, please verify you have the correct information and try again"))
-  }
-
-  userPaid(value) {
-    userData.stripe_token = value;
-    console.log("value of return: ", value);
-    console.log("user paid userDAta: ", userData);
-    this.setState({
-        id: userData.id,
-        email: userData.email,
-        password: userData.password,
-        firstName: userData.firstName,
-        lastName: userData.lastName,
-        middleName: userData.middleName
-      }); 
   }
 
   render() {
