4041a223406d3c55b5cda218059b6976399c2869
Gustav Nordstrom <gnordstrom@users.noreply.github.com>
2017-07-26 14:03:48 -0600

    Added a button and improved styling to Print

--- src/components/Print/Print.js
+++ src/components/Print/Print.js
@@ -3,8 +3,14 @@ import { Link, Redirect } from 'react-router-dom';
 import { getRegisteredUsers } from '../../dataService';
 import '../../styles/common.css';
 import '../pages/Classmates.css';
+import './Print.css';
 
 import Title from '../pages/Shared/Title';
+
+// Would you mind taking twenty minutes to make that component look nice and 
+// give me a pretty button at the top attached to an empty submit function? 
+// Then I'll fill that in with the stuff to actually cause the print. 
+// It's all on the `print` branch
 
 class Print extends Component {
   constructor(props) {
@@ -13,6 +19,8 @@ class Print extends Component {
     this.state = {
       registrants: []
     }
+
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentWillMount() {
@@ -23,12 +31,13 @@ class Print extends Component {
   }
 
   handleSubmit() {
+    console.log('Fire!');
   }
 
   render() {
-    let registrantList = this.state.registrants.map( registrant => {
+    let registrantList = this.state.registrants.map( (registrant, i) => {
       return (
-        <div>
+        <div key={i}>
           <p>{ registrant.first_name || '' } { registrant.last_name || ''}</p>
         </div>
 
@@ -37,13 +46,24 @@ class Print extends Component {
 
     return (
       <div className="component-wrapper">
-        <Title title="You can print all registrants here" />
+        {/* <Title title="You can print all registrants here"/> */}
+
+        <div className="title-top">
+                <div className="title-top-title">
+                  Click here to print out a list of all registrants
+                </div>
+                <div className="title-top-subtitle">
+                  <div className="print-button" onClick={this.handleSubmit}>Print</div>
+                </div>
+        </div> 
 
         <div className="classmates-table">
             <div className="table-header">
                 <div>Name</div>
             </div>
-            {registrantList}                
+            <div className="print-table-content">
+              {registrantList}                
+            </div>
         </div>
       </div>
     )
